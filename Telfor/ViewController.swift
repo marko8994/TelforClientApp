875cddd3bebbaafba5196c0f7265df9f6b19e6a8
Marko Mladenović <mmladenoviccloud@gmail.com>
2020-09-25 23:06:56 +0200

    Initial Commit

---
+++ Telfor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Telfor
+//
+//  Created by Marko Mladenovic on 25/09/2020.
+//  Copyright © 2020 Marko Mladenovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
